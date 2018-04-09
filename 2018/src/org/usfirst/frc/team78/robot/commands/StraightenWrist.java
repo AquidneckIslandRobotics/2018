9eac648079497f026fdc79a02f88eee8ea8496d6
acorey18 <alexandercorey662@yahoo.com>
2018-04-09 19:18:58 -0400

    init commit

--- 2018/src/org/usfirst/frc/team78/robot/commands/StraightenWrist.java
+++ 2018/src/org/usfirst/frc/team78/robot/commands/StraightenWrist.java
@@ -15,7 +15,7 @@ public class StraightenWrist extends Command {
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() {
+    protected void initialize() { 
     }
 
     // Called repeatedly when this Command is scheduled to run
